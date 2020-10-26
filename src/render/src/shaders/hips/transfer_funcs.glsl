5bb0c01fbd57d8d87c497925065bde4ed2ce466d
Matthieu Baumann <baumannmatthieu0@gmail.com>
2020-10-26 14:51:42 +0100

    transfer func

--- src/render/src/shaders/hips/transfer_funcs.glsl
+++ src/render/src/shaders/hips/transfer_funcs.glsl
@@ -1,55 +1,27 @@
+
 float linear_f(float x, float min_value, float max_value) {
-    if (x < min_value) {
-        return 0.0;
-    } else if (x > max_value) {
-        return 1.0;
-    } else {
-        return (x - min_value)/(max_value - min_value);
-    }
+    return clamp((x - min_value)/(max_value - min_value), 0.0, 1.0);
 }
 
 float sqrt_f(float x, float min_value, float max_value) {
-    if (x < min_value) {
-        return 0.0;
-    } else if (x > max_value) {
-        return 1.0;
-    } else {
-        return sqrt((x - min_value)/(max_value - min_value));
-    }
+    float a = linear_f(x, min_value, max_value);
+    return sqrt(a);
 }
 
 float log_f(float x, float min_value, float max_value) {
-    if (x < min_value) {
-        return 0.0;
-    } else if (x > max_value) {
-        return 1.0;
-    } else {
-        float d = (x - min_value)/(max_value - min_value);
-        float a = 1000.0;
-        return log(a*d + 1.0)/log(a);
-    }
+    float y = linear_f(x, min_value, max_value);
+    float a = 1000.0;
+    return log(a*y + 1.0)/log(a);
 }
 
 float asinh_f(float x, float min_value, float max_value) {
-    if (x < min_value) {
-        return 0.0;
-    } else if (x > max_value) {
-        return 1.0;
-    } else {
-        float d = (x - min_value)/(max_value - min_value);
-        return asinh(10.0*d)/3.0;
-    }
+    float d = linear_f(x, min_value, max_value);
+    return asinh(10.0*d)/3.0;
 }
 
 float pow2_f(float x, float min_value, float max_value) {
-    if (x < min_value) {
-        return 0.0;
-    } else if (x > max_value) {
-        return 1.0;
-    } else {
-        float d = (x - min_value)/(max_value - min_value);
-        return d*d;
-    }
+    float d = linear_f(x, min_value, max_value);
+    return d*d;
 }
 
 float transfer_func(int H, float x, float min_value, float max_value) {

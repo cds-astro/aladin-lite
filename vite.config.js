a80bff6289c1139808262bcf1b60b2de8d4b9af1
Matthieu Baumann <matthieu.baumann@astro.unistra.fr>
2023-05-26 13:48:57 +0200

    use the umd version for index.html

--- vite.config.js
+++ vite.config.js
@@ -41,8 +41,6 @@ export default defineConfig({
         cssInjectedByJsPlugin(),
     ],
     server: {
-        host: '0.0.0.0',
-        port: '8080',
         open: '/examples/index.html',
     },
 });

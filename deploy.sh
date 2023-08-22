c35c20d2419e5c600138655d0727671beb66ffd0
Matthieu Baumann <matthieu.baumann@astro.unistra.fr>
2023-08-22 14:50:46 +0200

    update the deploy

--- deploy.sh
+++ deploy.sh
@@ -13,4 +13,5 @@ ssh $USER_ALADIN@aladin "sg hips -c 'rm -rf /home/thomas.boch/AladinLite/www/api
 mkdir -p /home/thomas.boch/AladinLite/www/api/v3/$DATEUPLOAD && 
 cp /home/matthieu.baumann/al-tmp/* /home/thomas.boch/AladinLite/www/api/v3/$DATEUPLOAD && 
 rm -rf /home/thomas.boch/AladinLite/www/api/v3/latest && 
-ln -s /home/thomas.boch/AladinLite/www/api/v3/$DATEUPLOAD /home/thomas.boch/AladinLite/www/api/v3/latest'"
+ln -s /home/thomas.boch/AladinLite/www/api/v3/$DATEUPLOAD /home/thomas.boch/AladinLite/www/api/v3/latest &&
+ln -s /home/thomas.boch/AladinLite/www/api/v3/latest/aladin-lite.tgz /home/thomas.boch/AladinLite/www/api/v3/latest/AladinLiteAssets.tar.gz'"

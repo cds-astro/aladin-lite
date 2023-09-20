f4c32d9f0c92d32b56ece27ee23cb819af4af1d2
Matthieu Baumann <matthieu.baumann@astro.unistra.fr>
2023-09-20 19:33:09 +0200

    swap 2 dom elements code

--- src/js/gui/widgets/Utils.js
+++ src/js/gui/widgets/Utils.js
@@ -5,3 +5,14 @@ Element.prototype.insertChildAtIndex = function(child, index) {
         this.insertBefore(child, this.children[index])
     }
 };
+
+Element.prototype.swap = function (node) {
+    const parent = this.parentNode;
+    const sibling = this.nextSibling === node ? this : this.nextSibling;
+
+    // Move `this` to before the `node`
+    node.parentNode.insertBefore(this, node);
+
+    // Move `node` to before the sibling of `this`
+    parent.insertBefore(node, sibling);
+};

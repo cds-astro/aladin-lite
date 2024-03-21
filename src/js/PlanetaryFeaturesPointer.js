4ea8530528d80ba2d2be3260a5d4d95a5634096e
Thomas Boch <thomas.boch@astro.unistra.fr>
2024-03-21 11:31:13 +0100

    Add link to nominatim OSM when displaying result for earth "pointer"

--- src/js/PlanetaryFeaturesPointer.js
+++ src/js/PlanetaryFeaturesPointer.js
@@ -103,12 +103,18 @@ export let PlanetaryFeaturesPointer = (function() {
 
                     const featureFieldIdx = fields.findIndex((element) => element.includes('feature_name'));
                     const featureName = values[featureFieldIdx];
-                    const featureId = values[fields.indexOf('feature_id')]
+                    const featureId = values[fields.indexOf('feature_id')];
                     const title = '<div class="aladin-sp-title"><a target="_blank" href="https://planetarynames.wr.usgs.gov/Feature/' + featureId + '">' + featureName + '</a></div>';
                     const featureType = values[fields.indexOf('feature_type')]
                     let content = '<div class="aladin-sp-content">' + ' ' + '</div>';
                     content += '<em>Type: </em>' + featureType + '<br><br>';
-                    content += '<a target="_blank" href="https://planetarynames.wr.usgs.gov/Feature/' + featureId + '">More information</a>';
+                    let moreInfoURL = 'https://planetarynames.wr.usgs.gov/Feature/' + featureId;
+                    if (body === 'earth') {
+                        const placeId = values[fields.indexOf('place_id')];
+                        moreInfoURL = 'https://nominatim.openstreetmap.org/ui/details.html?place_id=' + placeId;
+                    }
+
+                    content += '<a target="_blank" href="' + moreInfoURL + '">More information</a>';
 
                     let lon = parseFloat(values[lonFieldIdx]);
                     const lat = parseFloat(values[latFieldIdx]);

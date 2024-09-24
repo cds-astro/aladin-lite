8a33084de9e218a06fcdd19109cf5cbeec38de01
Matthieu Baumann <matthieu.baumann@astro.unistra.fr>
2024-09-25 00:24:14 +0200

    make jsdoc a little more responsive

---
+++ jsdoc-make-responsive.js
@@ -0,0 +1,7 @@
+var meta = document.createElement("meta");
+meta.name = "viewport"
+meta.content = "width=device-width, height=device-height, initial-scale=1.0, user-scalable=no"
+
+document.querySelector("head").appendChild(
+    meta
+)

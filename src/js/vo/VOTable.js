2f2e3790b9cf8aaa759f810d2e1cf7f19877caff
Matthieu Baumann <matthieu.baumann@astro.unistra.fr>
2026-01-07 17:26:25 +0100

    change license from GPL3 to LGPL3

--- src/js/vo/VOTable.js
+++ src/js/vo/VOTable.js
@@ -1,20 +1,23 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright 2013 - UDS/CNRS
 // The Aladin Lite program is distributed under the terms
-// of the GNU General Public License version 3.
+// of the GNU Lesser General Public License version 3
+// or (at your option) any later version.
 //
 // This file is part of Aladin Lite.
 //
 //    Aladin Lite is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, version 3 of the License.
+//    it under the terms of the GNU Lesser General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
 //
 //    Aladin Lite is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU Lesser General Public License for more details.
 //
-//    The GNU General Public License is available in COPYING file
-//    along with Aladin Lite.
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with Aladin Lite. If not, see <https://www.gnu.org/licenses/>.
 //
 
 /******************************************************************************
@@ -22,225 +25,11 @@
  * 
  * File Catalog
  * 
- * Author: Thomas Boch[CDS]
+ * Author: Thomas Boch[CDS], Matthieu Baumann[CDS]
  * 
  *****************************************************************************/
 
 import { Utils } from "./../Utils";
-
-/*
-/// VOTable-rust version ///
-export class VOTable {
-    constructor(url, successCallback, errorCallback, useProxy) {
-        Utils.fetch({url, useProxy})
-            .then((resp) => resp.text())
-            .then((text) => {
-                ALEvent.AL_USE_WASM.dispatchedTo(document.body, {
-                    callback: (wasm) => {
-                        let votable;
-                        try {
-                            votable = wasm.parseVOTable(xml);
-                        } catch (e) {
-                            errorCallback('Catalogue failed to be parsed: ' + e);
-                        }
-                        
-                        votable.votable.get("resources")
-                            .forEach((rsc) => successCallback(rsc))
-                    }
-                })
-            })
-            .catch((e) => {
-                if (errorCallback) {
-                    errorCallback(e);
-                } else {
-                    throw e;
-                }
-            })
-    };
-
-    static parseTableRsc(rsc) {
-        let tables = rsc.get("tables")
-        if (tables) {
-            // take only the first table
-            let table = tables[0];
-        
-            let fields = table.get("elems")
-                .filter((elem) => {
-                    const elemType = elem["elem_type"] || elem.get("elem_type")
-                    return elemType === "Field";
-                })
-                .map((field) => {
-                    // convert a map into a javascript object
-                    return Object.fromEntries(field);
-                });
-
-            let data = table.get("data");
-            if (data) {
-                let rows;
-                if (data.get("data_type") === "TableData") {
-                    rows = data.get("rows");
-                } else if(data.get("data_type") === "Binary") {
-                    rows = data.get("stream")
-                        .get("rows");
-                } else {
-                    throw 'VOTable has data type not handled:' + data.get("data_type");
-                }
-
-                return {fields: fields, rows: rows};
-            }
-        }
-    };
-
-    static parseSODAServiceRsc(rsc) {
-        let isSODAService = rsc.get("utype") === "adhoc:service";
-        if (isSODAService) {
-            let elems = rsc.get("elems");
-            let id = rsc.get("ID");
-
-            if (id && id.includes("async")) {
-                // First way to check if the resource refers to a async SODA service
-                return;
-            }
-
-            if (elems) {
-                let accessUrl;
-                let inputParams = {};
-    
-                elems.forEach((elem) => {
-                    if (elem instanceof Map) {
-                        elem = Object.fromEntries(elem.entries());
-                    }
-
-                    // SODA access url
-                    if (elem["elem_type"] === "Param" && (elem["ucd"] === "meta.ref.url" || elem["name"] === "accessURL")) {
-                        accessUrl = elem["value"];
-                    } else if (elem["elem_type"] === "Param" && elem["name"] === "standardID") {
-                        // Check if it is the sync service
-                        // discard the async
-                        if (elem["value"].includes("async")) {
-                            return;
-                        }
-                    // Input params group
-                    } else if (elem["name"] === "inputParams") {
-                        elem["elems"].forEach((inputParam) => {
-                            let name = inputParam.get("name");
-                            let utype = inputParam.get("utype");
-                            let values;
-                            switch (name) {
-                                case 'ID':
-                                    inputParams['ID'] = {
-                                        name: 'ID',
-                                        type: 'group',
-                                        description: inputParam.get("description"),
-                                        subInputs: [{
-                                            label: "ID",
-                                            name: 'ID',
-                                            type: "text",
-                                            value: inputParam.get("value")
-                                        }],
-                                    }
-                                    break;
-                                case 'CIRCLE':
-                                    if (inputParam.get("values")) {
-                                        values = inputParam.get("values")["max"]["value"].split(" ").map((v) => {return +v;});
-                                    }
-
-                                    inputParams['Circle'] = {
-                                        name: 'CIRCLE',
-                                        type: 'group',
-                                        description: inputParam.get("description"),
-                                        subInputs: [{
-                                            name: 'ra',
-                                            label: 'ra[' + utype + ']',
-                                            type: 'number',
-                                            value: values && values[0],
-                                        },
-                                        {
-                                            name: 'dec',
-                                            label: 'dec[' + utype + ']',
-                                            type: 'number',
-                                            value: values && values[1],
-                                        },
-                                        {
-                                            name: 'rad',
-                                            label: 'rad[' + utype + ']',
-                                            type: 'number',
-                                            value: values && values[2],
-                                        }]
-                                    };
-                                    break;
-                                case 'BAND':
-                                    if (inputParam.get("values")) {
-                                        values = inputParam.get("values")["max"]["value"].split(" ").map((v) => {return +v;});
-                                    }
-
-                                    inputParams['Band'] = {
-                                        name: 'BAND',
-                                        type: 'group',
-                                        description: inputParam.get("description"),
-                                        subInputs: [{
-                                            name: 'fmin',
-                                            label: 'fmin[' + utype + ']',
-                                            type: 'number',
-                                            value: values && values[0],
-                                        },
-                                        {
-                                            name: 'fmax',
-                                            label: 'fmax[' + utype + ']',
-                                            type: 'number',
-                                            value: values && values[1],
-                                        }]
-                                    };
-                                    break;
-                                case 'RANGE':
-                                    if (inputParam.get("values")) {
-                                        values = inputParam.get("values")["max"]["value"].split(" ").map((v) => {return +v;});
-                                    }    
-                                    inputParams['Range'] = {
-                                        name: 'RANGE',
-                                        type: 'group',
-                                        description: inputParam.get("description"),
-                                        subInputs: [{
-                                            name: 'ramin',
-                                            label: 'ramin[' + utype + ']',
-                                            type: 'number',
-                                            value: values && values[0],
-                                        },
-                                        {
-                                            name: 'ramax',
-                                            label: 'ramax[' + utype + ']',
-                                            type: 'number',
-                                            value: values && values[1],
-                                        },
-                                        {
-                                            name: 'decmin',
-                                            label: 'decmin[' + utype + ']',
-                                            type: 'number',
-                                            value: values && values[2],
-                                        },
-                                        {
-                                            name: 'decmax',
-                                            label: 'decmax[' + utype + ']',
-                                            type: 'number',
-                                            value: values && values[3],
-                                        }]
-                                    };
-                                default:
-                                    break;
-                            }
-                        })
-                    }
-                })
-    
-                return {
-                    baseUrl: accessUrl,
-                    inputParams: inputParams,
-                }
-            }
-        }
-    };
-};
-*/
 
 /// Pure Vanilla JS version (lighter)
 export class VOTable {
